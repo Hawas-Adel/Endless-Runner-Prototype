bb6bbe2db6ae99be93836d0bf836cd7588d157b0
Hawas-Adel <Hawas-Adel@users.noreply.github.com>
2020-09-22 22:07:59 +0200

    Added Speed boost platform

---
+++ Assets/Scripts/Boost Platform/BoostPlatform.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SelectionBase]
+[RequireComponent(typeof(Collider))]
+public class BoostPlatform : MonoBehaviour
+{
+	[SerializeField] [Min(0)] private float ForceMagnitude = 100;
+
+	private void OnTriggerEnter(Collider other) => other.attachedRigidbody.AddRelativeForce(0, 0, ForceMagnitude, ForceMode.Impulse);
+}
