31c0523d262d0f1ef7e2d37f6efc942f1dba419e
Hawas-Adel <Hawas-Adel@users.noreply.github.com>
2020-09-24 14:59:12 +0200

    Added Coins

---
+++ Assets/Scripts/Coins/Coin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class Coin : MonoBehaviour
+{
+	private void OnTriggerEnter(Collider other)
+	{
+		var collector = other.GetComponentInParent<CoinCollector>();
+		if (collector)
+		{
+			collector.CoinsCoint++;
+			Debug.Log($"{collector.name} has {collector.CoinsCoint} Coins");
+			gameObject.SetActive(false);
+		}
+	}
+}
