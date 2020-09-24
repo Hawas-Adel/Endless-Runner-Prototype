d55306441b92b7aff1731ecd258c457358b70a96
Hawas-Adel <Hawas-Adel@users.noreply.github.com>
2020-09-24 23:15:01 +0200

    Added Main Menu

---
+++ Assets/Scripts/Main Menu/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using NaughtyAttributes;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+	[SerializeField] [Scene] private string StartSceneREF = default;
+
+	[SerializeField] private RectTransform MainMenuREF = default;
+	[SerializeField] private RectTransform SettingsMenuREF = default;
+
+	public void GoToScene() => SceneManager.LoadScene(StartSceneREF);
+	public void MainMenuToSettings()
+	{
+		MainMenuREF.gameObject.SetActive(false);
+		SettingsMenuREF.gameObject.SetActive(true);
+	}
+	public void SettingsToMainMenu()
+	{
+		SettingsMenuREF.gameObject.SetActive(false);
+		MainMenuREF.gameObject.SetActive(true);
+	}
+	public void Exit()
+	{
+		Application.Quit();
+#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+#endif
+	}
+}
